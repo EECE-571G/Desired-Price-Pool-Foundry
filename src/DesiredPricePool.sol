2c9132823bcca7081009fbbb9b3a1ede018cd7ff
jadrvel <jadrvel@outlook.com>
2025-04-06 22:03:35 -0700

    feat(contract): new hook fee formula

--- src/DesiredPricePool.sol
+++ src/DesiredPricePool.sol
@@ -12,11 +12,12 @@ import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {Currency} from "v4-core/src/types/Currency.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BalanceDelta, BalanceDeltaLibrary, toBalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary, toBeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
 
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import {Owned} from "solmate/src/auth/Owned.sol";
 
@@ -29,10 +30,12 @@ function abs(int256 x) pure returns (int256) {
 }
 
 contract DesiredPricePool is BaseHook, Owned {
-    using PoolIdLibrary for PoolKey;
-    using CustomRevert for bytes4;
     using StateLibrary for IPoolManager;
+    using PoolIdLibrary for PoolKey;
+    using BalanceDeltaLibrary for BalanceDelta;
+    using CustomRevert for bytes4;
     using SafeCast for uint256;
+	using SafeCast for int256;
 
     error UnauthorizedPoolInitialization();
     error UnexpectedReentrancy();
@@ -41,8 +44,10 @@ contract DesiredPricePool is BaseHook, Owned {
     uint24 public constant DEFAULT_BASE_FEE_PER_TICK = 30;
     /// @notice The percentage of hook fees on top of the base fee.
     uint8 public constant DEFAULT_HOOK_FEE = 25;
+	/// @notice The maximum tick spacing for the pool.
+	int24 public constant MAX_TICK_SPACING = 200;
 
-    int24 private BEFORE_SWAP_TICK_UNSET = TickMath.MAX_TICK + 1;
+    int24 private BEFORE_SWAP_TICK_ZERO = TickMath.MAX_TICK + 1;
 
     // NOTE: ---------------------------------------------------------
     // state variables should typically be unique to a pool
@@ -53,8 +58,8 @@ contract DesiredPricePool is BaseHook, Owned {
     mapping(PoolId => uint24 pip) public baseFees;
     mapping(PoolId => uint8 percent) public hookFees;
 
-    int24 private beforeSwapTick = BEFORE_SWAP_TICK_UNSET;
-    uint24 private swapFee = 0;
+    int24 private _beforeSwapTick;
+    uint24 private _swapFee;
 
     constructor(IPoolManager _poolManager, address _owner) BaseHook(_poolManager) Owned(_owner) {}
 
@@ -66,7 +71,7 @@ contract DesiredPricePool is BaseHook, Owned {
         int24 _desiredPriceTick
     ) external onlyOwner {
         require(!(_currency0 == _currency1), "Invalid currency pair");
-        require(_tickSpacing >= 1 && _tickSpacing <= 200, "Invalid tick spacing");
+        require(_tickSpacing >= 1 && _tickSpacing <= MAX_TICK_SPACING, "Invalid tick spacing");
 
         if (_currency0 > _currency1) {
             (_currency0, _currency1) = (_currency1, _currency0);
@@ -120,17 +125,17 @@ contract DesiredPricePool is BaseHook, Owned {
         override
         returns (bytes4, BeforeSwapDelta, uint24)
     {
-        if (beforeSwapTick != BEFORE_SWAP_TICK_UNSET) {
+        if (_beforeSwapTick != 0) {
             UnexpectedReentrancy.selector.revertWith();
         }
         PoolId id = key.toId();
-        uint24 protocolFee;
-        (, beforeSwapTick, protocolFee,) = poolManager.getSlot0(id);
+        (, int24 currentTick, uint24 protocolFee,) = poolManager.getSlot0(id);
+		_beforeSwapTick = currentTick == 0 ? BEFORE_SWAP_TICK_ZERO : currentTick;
         uint16 currentProtocolFee = swapParams.zeroForOne
             ? ProtocolFeeLibrary.getZeroForOneFee(protocolFee)
             : ProtocolFeeLibrary.getOneForZeroFee(protocolFee);
         uint24 fee = baseFees[id];
-        swapFee = ProtocolFeeLibrary.calculateSwapFee(currentProtocolFee, fee);
+        _swapFee = ProtocolFeeLibrary.calculateSwapFee(currentProtocolFee, fee);
         return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, fee | LPFeeLibrary.OVERRIDE_FEE_FLAG);
     }
 
@@ -141,30 +146,41 @@ contract DesiredPricePool is BaseHook, Owned {
         BalanceDelta delta,
         bytes calldata
     ) internal override returns (bytes4, int128) {
-        if (beforeSwapTick == BEFORE_SWAP_TICK_UNSET) {
+		int24 beforeSwapTick = _beforeSwapTick;
+		uint256 swapFee = _swapFee;
+		_beforeSwapTick = 0;
+		_swapFee = 0;
+        if (beforeSwapTick == 0) {
             UnexpectedReentrancy.selector.revertWith();
         }
+		beforeSwapTick = beforeSwapTick == BEFORE_SWAP_TICK_ZERO ? int24(0) : beforeSwapTick;
+
         PoolId id = key.toId();
-        int128 deltaUnspecified =
-            swapParams.zeroForOne == (swapParams.amountSpecified < 0) ? delta.amount1() : delta.amount0();
+        bool chargeCurrency1 = swapParams.zeroForOne == (swapParams.amountSpecified < 0);
+        int128 deltaUnspecified = chargeCurrency1 ? delta.amount1() : delta.amount0();
         uint24 baseFee = baseFees[id];
         uint24 hookFeePip = baseFee * hookFees[id] / 100;
-        int128 hookFeeAmountBase = SafeCast.toInt128(
-            abs(int256(deltaUnspecified)) * (1e6 - SafeCast.toInt256(swapFee)) * SafeCast.toInt256(hookFeePip) / 1e12
-        );
+        uint256 hookFeeAmountBase =
+            abs(int256(deltaUnspecified)).toUint256() * (1e6 - swapFee) * uint256(hookFeePip) / 1e12;
 
         (, int24 afterSwapTick,,) = poolManager.getSlot0(id);
         int24 desiredPrice = desiredPriceTicks[id];
         int24 tickDiff = abs24(afterSwapTick - desiredPrice) - abs24(beforeSwapTick - desiredPrice);
-        beforeSwapTick = BEFORE_SWAP_TICK_UNSET;
 
+        uint256 hookFeeAmount; // 1 / (1 + tickDiff / (4 * sqrt(tickSpacing)))
         if (tickDiff == 0) {
-            return (BaseHook.afterSwap.selector, hookFeeAmountBase);
+            hookFeeAmount = hookFeeAmountBase;
+        } else {
+            uint256 factor = Math.sqrt(int256(key.tickSpacing).toUint256() << 232) << 2;
+            hookFeeAmount = hookFeeAmountBase * factor / (factor + (int256(abs24(tickDiff)).toUint256() << 116));
+            if (tickDiff > 0) {
+                hookFeeAmount = (hookFeeAmountBase << 1) - hookFeeAmount;
+            }
         }
-        int128 hookFeeAmount = tickDiff < 0
-            ? ((hookFeeAmountBase << 1) * (tickDiff - 1) / (tickDiff - 2))
-            : ((hookFeeAmountBase << 1) / (2 + tickDiff));
-        return (BaseHook.afterSwap.selector, hookFeeAmount);
+		int128 hookFeeAmountInt128 = hookFeeAmount.toInt256().toInt128();
+        BalanceDelta hookDelta = chargeCurrency1 ? toBalanceDelta(0, hookFeeAmountInt128) : toBalanceDelta(hookFeeAmountInt128, 0);
+        feesCollected[id] = feesCollected[id] + hookDelta;
+        return (BaseHook.afterSwap.selector, hookFeeAmountInt128);
     }
 
     function _afterAddLiquidity(
