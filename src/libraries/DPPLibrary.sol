b9162747cd26e7145de6999e1edf45c798f10064
jadrvel <jadrvel@outlook.com>
2025-04-12 17:12:31 -0700

    feat(test): add `DPPLibrary`

---
+++ src/libraries/DPPLibrary.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+
+library DPPLibrary {
+    /// @notice Address suffix of the hook contract.
+    uint160 internal constant PERMISSION_FLAGS =
+        Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
+            | Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG;
+}
